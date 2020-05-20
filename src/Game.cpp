822e6a381275399e5a545e8a84614185014adbce
AzamatNabiullin <azamat.nabiullin.97@mail.ru>
2020-05-20 15:48:21 +0300

    Added food and score

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,6 +6,8 @@ bool Game::OnUserCreate()
 
 	mGameState = GameState::MENU;
 
+	SpawnFood(FoodType::APPLE);
+
 	return true;
 }
 
@@ -16,9 +18,13 @@ bool Game::OnUserUpdate(float delta)
 	switch (mGameState)
 	{
 	case GameState::MENU:
+
+		Clear(olc::GREY);
 		DrawMenuScreen();
+
 		break;
 	case GameState::ACTIVE:
+
 		//limit the snake speed
 		if (mCurrentFrameNum % mGameSpeed == 0)
 		{
@@ -30,14 +36,19 @@ bool Game::OnUserUpdate(float delta)
 
 		Clear(olc::GREY);
 
+		DrawFood();
 		DrawSnake();
+		DrawScore(olc::vf2d(ScreenWidth() - 100.0f, 50.0f), olc::vf2d(4, 4));
 
 		mCurrentFrameNum++;
+
 		break;
 	case GameState::GAME_OVER:
+
 		Clear(olc::GREY);
 
 		DrawGameOverScreen();
+
 		break;
 	default:
 		break;
@@ -48,6 +59,7 @@ bool Game::OnUserUpdate(float delta)
 
 void Game::ProcessInput()
 {
+	//Horizontal input
 	if (GetKey(olc::D).bPressed)
 	{
 		//restrict the snake from going inside itself
@@ -75,6 +87,7 @@ void Game::ProcessInput()
 		}
 	}
 
+	//Vertical input
 	if (GetKey(olc::W).bPressed)
 	{
 		//restrict the snake from going inside itself
@@ -102,11 +115,18 @@ void Game::ProcessInput()
 		}
 	}
 
+	//Other
 	if (GetKey(olc::ENTER).bPressed)
 	{
 		mSnake.reset();
 		CreateSnake();
+
+		mFood.clear();
+
+		SpawnFood(FoodType::APPLE);
+
 		mGameState = GameState::ACTIVE;
+		mCurrentScore = 0;
 	}
 }
 
@@ -135,13 +155,35 @@ void Game::ResolveSnakeCollisions()
 	//Check collisions against the walls
 	if (
 		head.currentPos.x < 0.0f ||
-		head.currentPos.x + mSnakeSize.x > ScreenWidth() ||
+		head.currentPos.x + mBlockSize.x > ScreenWidth() ||
 		head.currentPos.y < 0.0f ||
-		head.currentPos.y + mSnakeSize.y > ScreenHeight()
+		head.currentPos.y + mBlockSize.y > ScreenHeight()
 		)
 	{
 		mGameState = GameState::GAME_OVER;
 		return;
+	}
+
+	//Check collisions against food
+	for (int i = 0; i < mFood.size(); i++)
+	{
+		if (mFood[i].position.x == head.currentPos.x && mFood[i].position.y == head.currentPos.y)
+		{
+			//increase score
+			mCurrentScore += mFood[i].points;
+
+			//create new snake block
+			olc::vf2d talePosition{ mSnake->back().currentPos };
+			mSnake->push_back(
+				SnakeBlock{
+					olc::vf2d(talePosition.x + mSnakeDirection.x * mBlockSize.x, talePosition.y + mSnakeDirection.y * mBlockSize.y),
+					olc::vf2d(talePosition.x + mSnakeDirection.x * mBlockSize.x, talePosition.y + mSnakeDirection.y * mBlockSize.y)
+				});
+
+			//erase eaten food and spawn a new one
+			mFood.erase(mFood.begin() + i);
+			SpawnFood(FoodType::APPLE);
+		}
 	}
 }
 
@@ -152,6 +194,8 @@ void Game::DrawGameOverScreen()
 	DrawStringDecal(olc::vf2d(screenSize.x / 2 - screenSize.x / 6, screenSize.y / 2 - screenSize.y / 6), "GAME OVER", olc::DARK_RED, olc::vf2d(8, 8));
 	//second line is positioned below the GAME OVER line
 	DrawStringDecal(olc::vf2d(screenSize.x / 2 - screenSize.x / 4, screenSize.y / 2 - screenSize.y / 12), "Press 'Enter' to start again", olc::DARK_RED, olc::vf2d(4, 4));
+
+	DrawScore(olc::vf2d(screenSize.x / 2, screenSize.y / 2 + screenSize.y / 4), olc::vf2d(8, 8));
 }
 
 void Game::DrawMenuScreen()
@@ -163,6 +207,13 @@ void Game::DrawMenuScreen()
 	DrawStringDecal(olc::vf2d(screenSize.x / 2 - screenSize.x / 4.5, screenSize.y / 2 - screenSize.y / 12), "Press 'Enter' to start", olc::DARK_RED, olc::vf2d(4, 4));
 }
 
+void Game::DrawScore(olc::vf2d position, olc::vf2d size)
+{
+	olc::vf2d screenSize{ static_cast<float>(ScreenWidth()), static_cast<float>(ScreenHeight()) };
+
+	DrawStringDecal(position, std::to_string(mCurrentScore), olc::DARK_RED, size);
+}
+
 void Game::DrawSnake()
 {
 	bool first = true;
@@ -172,16 +223,16 @@ void Game::DrawSnake()
 		//draw head
 		if (first)
 		{
-			DrawRect(block.currentPos, mSnakeSize, olc::RED);
-			FillRect(block.currentPos, mSnakeSize, olc::RED);
+			DrawRect(block.currentPos, mBlockSize, olc::RED);
+			FillRect(block.currentPos, mBlockSize, olc::RED);
 			first = false;
 
 			continue;
 		}
 
 		//draw body
-		DrawRect(block.currentPos, mSnakeSize, olc::GREEN);
-		FillRect(block.currentPos, mSnakeSize, olc::GREEN);
+		DrawRect(block.currentPos, mBlockSize, olc::GREEN);
+		FillRect(block.currentPos, mBlockSize, olc::GREEN);
 	}
 }
 
@@ -196,8 +247,8 @@ void Game::MoveSnake(float delta)
 		if (first)
 		{
 			block.previousPos = block.currentPos;
-			block.currentPos.x += mSnakeSize.x * mSnakeDirection.x;
-			block.currentPos.y += mSnakeSize.y * mSnakeDirection.y;
+			block.currentPos.x += mBlockSize.x * mSnakeDirection.x;
+			block.currentPos.y += mBlockSize.y * mSnakeDirection.y;
 
 			previousBlock = block;
 
@@ -219,11 +270,54 @@ void Game::CreateSnake()
 	mSnake = std::make_unique<std::list<SnakeBlock>>();
 
 	//initialize the snake
-
-	for (int i = mSnakeSize.x * 5 + mStartingPos.x; i > mStartingPos.x; i -= mSnakeSize.x)
+	for (int i = mBlockSize.x * 5 + mStartingPos.x; i > mStartingPos.x; i -= mBlockSize.x)
 	{
 		mSnake->push_back(SnakeBlock{ olc::vf2d(i, mStartingPos.y), olc::vf2d(i, mStartingPos.y) });
 	}
 
 	mSnakeDirection = olc::vf2d(1, 0);
+}
+
+void Game::SpawnFood(FoodType type)
+{
+	srand(time(NULL));
+
+	olc::vf2d position{ float(rand() % (ScreenWidth() / 32) * 32), float(rand() % (ScreenHeight() / 32) * 32) };
+
+	while (position.x <= 0.0f || position.x + mBlockSize.x >= ScreenWidth())
+	{
+		position.x = float(rand() % (ScreenWidth() / 32) * 32);
+	}
+
+	while (position.y <= 0.0f || position.y + mBlockSize.y >= ScreenHeight())
+	{
+		position.y = float(rand() % (ScreenHeight() / 32) * 32);
+	}
+
+	olc::Pixel color;
+
+	switch (type)
+	{
+	case FoodType::APPLE:
+		color = olc::RED;
+		break;
+	case FoodType::ORANGE:
+		color = olc::DARK_BLUE;
+		break;
+	default:
+		break;
+	}
+
+	Food food{ position, int(type), color };
+
+	mFood.push_back(food);
+}
+
+void Game::DrawFood()
+{
+	for (const auto& food : mFood)
+	{
+		DrawRect(food.position, mBlockSize, food.color);
+		FillRect(food.position, mBlockSize, food.color);
+	}
 }
--- src/Game.h
+++ src/Game.h
@@ -3,6 +3,7 @@
 
 #include <list>
 #include <memory>
+#include <Windows.h>
 
 #include "olcPixelGameEngine.h"
 
@@ -10,6 +11,20 @@ struct SnakeBlock
 {
 	olc::vf2d previousPos;
 	olc::vf2d currentPos;
+};
+
+struct Food
+{
+	olc::vf2d position;
+	//how much points are awarded
+	int points;
+	olc::Pixel color;
+};
+
+enum class FoodType
+{
+	APPLE = 1,
+	ORANGE
 };
 
 enum class GameState
@@ -34,22 +49,34 @@ public:
 	void ResolveSnakeCollisions();
 	void DrawGameOverScreen();
 	void DrawMenuScreen();
+	void DrawScore(olc::vf2d position, olc::vf2d size);
 
 	//Snake manipulation methods
 	void DrawSnake();
 	void MoveSnake(float delta);
 	void CreateSnake();
+
+	void SpawnFood(FoodType type);
+	void DrawFood();
 private:
 	GameState mGameState;
 
 	std::unique_ptr<std::list<SnakeBlock>> mSnake;
+	std::vector<Food> mFood;
+	olc::vf2d mFoodSize{ 32, 32 };
+	//the higher the speed the slower food will spawn
+	int mFoodSpawningSpeed = 240;
 
-	olc::vf2d mStartingPos{ 150, 150 };
+	olc::vf2d mStartingPos{ 160, 160 };
 	olc::vf2d mSnakeSize{ 32, 32 };
 	olc::vf2d mSnakeDirection{ 1, 0 };
 
+	olc::vf2d mBlockSize{ 32, 32 };
+
 	//the higher the mGameSpeed the slower the game will run
-	int mGameSpeed = 12;
+	int mGameSpeed = 4;
 	int mCurrentFrameNum = 0;
+
+	int mCurrentScore = 0;
 };
 #endif
